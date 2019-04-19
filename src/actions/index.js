3f6ac930b341acf620343b4d06365363e4c2a9ee
Stephen Buchanan <stephen.buchanan@random.studio>
2019-04-19 15:42:12 +0200

    tidy up

--- src/actions/index.js
+++ src/actions/index.js
@@ -46,50 +46,20 @@ const addConnection = (connection, widgets) => {
 }
 
 export default {
-  add: (/* event (e) */) => ({ num }) => ({ num: num + 1 }),
-  // sub: (/* event (e) */) => ({ num }) => ({ num: num - 1 }),
-
   createNetwork: ({newWidgets, newConnections}) => ({widgets, connections}) => {
 
     console.log(`creating network with ${newWidgets.length} widget(s) and ${newConnections.length} connection(s)...`);
 
+    // First, the side effects...
     let widgetInstances = newWidgets.map(newWidget => addWidget(newWidget));
     let connectionInstances = newConnections.map(newConnection => addConnection(newConnection, widgetInstances));
 
+    // Then return the objects (with their "toneRef" property attached) into the state tree...
     return {
       widgets: widgetInstances,
       connections: connectionInstances
     };
 
-    // widgets.forEach(widget => {
-    //   switch(widget.type) {
-    //       case 'Tone.Master':
-    //           // created on initialisation by ToneJS
-    //       break;
-
-    //       case 'Tone.Oscillator':
-    //           console.log('create oscillator');
-    //           let osc = new Tone.Oscillator();
-    //           widgetsList.push(osc);
-    //       break;
-
-    //       default:
-    //           console.error('unknown widget.type for:', widget);
-    //   }
-    // });
-
-  //   connections.forEach(connection => {
-  //       const source = widgets.find(w => w.id === connection.from.id);
-  //       const destination = widgets.find(w => w.id === connection.to.id);
-
-  //       if (destination === undefined || source === undefined) {
-  //           console.error('cannot find source and/or destination for connection', connection);
-  //       } else {
-  //           if (isMaster(destination)) {
-  //               source.connect(Tone.Master);
-  //           }
-  //       }
-  //   });
   },
 
   startAll: () => ({ widgets }) => {
@@ -98,7 +68,8 @@ export default {
     
     startableWidgets.forEach(w => w.toneRef.start());
 
-    return { ...widgets };
+    // Side effects only here...
+    return widgets;
   },
 
   stopAll: () => ({ widgets }) => {
@@ -107,7 +78,8 @@ export default {
     
     stoppableWidgets.forEach(w => w.toneRef.stop());
 
-    return { ...widgets };
+    // Side effects only here...
+    return widgets;
   }
 
 };
