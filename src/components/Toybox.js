a74f250be47b5e05f0ab224c82b4448f5fc11fb2
Stephen Buchanan <stephen.buchanan@random.studio>
2019-04-19 15:31:44 +0200

    easier JSON debugging

--- src/components/Toybox.js
+++ src/components/Toybox.js
@@ -2,12 +2,13 @@ import { h } from 'hyperapp';
 
 const isMaster = (widget) => widget.type === 'Tone.Master'
 
+const widgetJSON = (widgets) => JSON.stringify(widgets.map(w => ({ ...w, toneRef: '_hidden'})));
 
 export default ({widgets, connections, start, stop}) => (
     <div>
       <h2>Toybox</h2>
       <div>
-            widgets.length: {widgets.length}; 
+            widgets: {widgetJSON(widgets)}; 
             connections: {JSON.stringify(connections)}
       </div>
       <div>
