d269965f6d67b4b91cbd6c37ba018f895ea33177
Stephen Buchanan <stephen.buchanan@random.studio>
2019-04-19 14:24:16 +0200

    lifecycle init boilerplate

--- src/components/Toybox.js
+++ src/components/Toybox.js
@@ -1,7 +1,11 @@
 import { h } from 'hyperapp';
 
+const createNetwork = ({ widgets, connections }) => {
+    console.log(`creating network with ${widgets.length} widget(s) and ${connections.length} connection(s)...`);
+}
+
 export default ({widgets, connections}) => (
-    <div>
+    <div oncreate={() => createNetwork({widgets, connections})}>
       <h2>Toybox</h2>
       <div>
           <h3>Widgets</h3>
